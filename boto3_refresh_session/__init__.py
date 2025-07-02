d1b25a97543e017e9c50be2875b18611826322d2
Mike Letts <michaelthomasletts@gmail.com>
2025-07-01 20:30:25 -0700

    double underscore updates to __init__

--- boto3_refresh_session/__init__.py
+++ boto3_refresh_session/__init__.py
@@ -6,3 +6,6 @@ from .sts import STSRefreshableSession
 __all__ = ["RefreshableSession"]
 __version__ = "1.3.12"
 __author__ = "Mike Letts"
+__maintainer__ = "Mike Letts"
+__license__ = "MIT"
+__email__ = "lettsmt@gmail.com"
