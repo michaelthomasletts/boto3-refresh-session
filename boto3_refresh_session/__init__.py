108ea26a0ef13aa1fc2a86eb955e19d556d979dd
Mike Letts <47342651+michaelthomasletts@users.noreply.github.com>
2025-08-22 19:15:20 -0400

    bug fix
    The way I was updating __version__ in __init__ was completely overwriting __init__.

--- boto3_refresh_session/__init__.py
+++ boto3_refresh_session/__init__.py
@@ -1,1 +1,9 @@
-__version__ = '2.0.4'
+from .session import RefreshableSession
+
+__all__ = ["RefreshableSession"]
+__version__ = "2.0.4"
+__title__ = "boto3-refresh-session"
+__author__ = "Mike Letts"
+__maintainer__ = "Mike Letts"
+__license__ = "MIT"
+__email__ = "lettsmt@gmail.com"
