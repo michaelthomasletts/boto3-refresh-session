9f19d4e10a41f51493cb615495efa01565ca8cbb
Mike Letts <47342651+michaelthomasletts@users.noreply.github.com>
2026-01-26 00:09:04 -0500

    deprecating and rewriting constants
    This commit removes ROLE_ARN_PATTERN, MFA_SERIAL_PATTERN, and ROLE_SESSION_NAME_PATTERN, and fetches the keys for ASSUME_ROLE_CONFIG_PARAMETERS and STS_CLIENT_CONFIG_PARAMETERS from AssumeRoleParams and STSClientParams for simplicity

--- boto3_refresh_session/utils/constants.py
+++ boto3_refresh_session/utils/constants.py
@@ -4,38 +4,8 @@
 
 __all__ = ["ASSUME_ROLE_CONFIG_PARAMETERS", "STS_CLIENT_CONFIG_PARAMETERS"]
 
-from re import compile
-
-# THESE CONSTANTS WILL BE DEPRECATED IN A FUTURE RELEASE!!
-ROLE_ARN_PATTERN = compile(r"^arn:aws[a-z-]*:iam::\d{12}:role/[\w+=,.@-]+$")
-MFA_SERIAL_PATTERN = compile(r"^arn:aws[a-z-]*:iam::\d{12}:mfa/[\w+=,.@-]+$")
-ROLE_SESSION_NAME_PATTERN = compile(r"^[a-zA-Z0-9+=,.@-]{2,64}$")
+from .typing import AssumeRoleParams, STSClientParams
 
 # config parameter names
-ASSUME_ROLE_CONFIG_PARAMETERS = (
-    "RoleArn",
-    "RoleSessionName",
-    "PolicyArns",
-    "Policy",
-    "DurationSeconds",
-    "ExternalId",
-    "SerialNumber",
-    "TokenCode",
-    "Tags",
-    "TransitiveTagKeys",
-    "SourceIdentity",
-    "ProvidedContexts",
-)
-STS_CLIENT_CONFIG_PARAMETERS = (
-    "service_name",
-    "region_name",
-    "api_version",
-    "use_ssl",
-    "verify",
-    "endpoint_url",
-    "aws_access_key_id",
-    "aws_secret_access_key",
-    "aws_session_token",
-    "config",
-    "aws_account_id",
-)
+ASSUME_ROLE_CONFIG_PARAMETERS = tuple(AssumeRoleParams.__annotations__)
+STS_CLIENT_CONFIG_PARAMETERS = tuple(STSClientParams.__annotations__)
